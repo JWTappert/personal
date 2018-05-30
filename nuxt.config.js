8405d9e3f915605620fe72551e35df7c30a59693
JWTappert <justin@theflybook.com>
2018-05-29 22:42:00 -0700

    first go

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,18 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'app',
+    title: "///_",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,15 +22,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <!-- <app-logo/> -->
       <h1 class="title">
-        app
+        <span>デ</span> ン プ ス <span>タ</span> ー
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+       <a href="https://github.com/JWTappert">d e r p s t a r r</a>
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+       derp
       </div>
     </div>
   </section>
@@ -44,21 +37,71 @@ export default {
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
-  font-weight: 300;
   font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  letter-spacing: 5px;
+	-webkit-text-stroke: 1px white;
+  color: white;
+  text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #FF1D8E, 0 0 0.5em #FF1D8E, 0 0 0.1em #FF1D8E, 0 10px 3px #000;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
+  color: white;
+  word-spacing: 50px;
   padding-bottom: 15px;
+  text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #FF1D8E, 0 0 0.5em #FF1D8E, 0 0 0.1em #FF1D8E, 0 10px 3px #000;
+}
+
+.subtitle a {
+  color: inherit;
+  text-decoration: inherit;
 }
 
 .links {
   padding-top: 15px;
 }
+
+body {
+  background-color: black;
+}
+
+.title span {
+  animation: blink linear 2s infinite;
+}
+
+@keyframes blink {
+  78% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+  79%{
+     color: #333;
+  }
+  80% {
+
+    text-shadow: none;
+  }
+  81% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+  82% {
+    color: #333;
+    text-shadow: none;
+  }
+  83% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+  92% {
+    color: #333;
+    text-shadow: none;
+  }
+  92.5% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+}
+
 </style>
