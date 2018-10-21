2cf50d53e51b3cb66b1f5a6255c3571c456a8aaa
JWTappert <justin@theflybook.com>
2018-10-21 13:06:13 -0700

    get random pastel color

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
+  @Input()
+  color: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/name-svg/name-svg.component.ts
+++ src/app/name-svg/name-svg.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-name-svg",
@@ -6,6 +6,9 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./name-svg.component.css"]
 })
 export class NameSvgComponent implements OnInit {
+  @Input()
+  color: string;
+
   constructor() {}
 
   ngOnInit() {}
