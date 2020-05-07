0c541dd62ba03ce463bd8303ca12fde892988fd7
Tappert <tappertwj@gmail.com>
2020-05-06 21:34:41 -0700

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,54 @@
+import { useState } from "react";
+import React from "react";
+import useHorizontal from "@oberon-amsterdam/horizontal/hook";
+
 import Head from "next/head";
 import Name from "../components/name/name";
-import Nav from "../components/nav/nav";
+import GithubIcon from "../components/icons/github";
+import LinkedInIcon from "../components/icons/linkedin";
 
 export default function Home() {
+  const [container, setContainer] = useState();
+  useHorizontal({ container: container });
+
   return (
-    <div className="container">
+    <React.Fragment>
       <Head>
         <title>d e r p s t a r r</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Name />
-      <Nav />
-    </div>
+      <header>
+        <div className="brand">
+          <Name />
+          <small>Software & Design</small>
+        </div>
+
+        <div className="social">
+          <GithubIcon />
+          <LinkedInIcon />
+        </div>
+      </header>
+      <div
+        className="container"
+        ref={(ref) => {
+          setContainer(ref);
+        }}
+      >
+        <div className="block" id="intro">
+          Hey, I'm Justin. I'm a software engineer and I have a passion for art
+          and design.
+        </div>
+
+        <div className="block">Why hello there</div>
+
+        <div className="block">
+          <a href="react.html">See react example</a>
+        </div>
+
+        <div className="block">
+          <a href="#intro">Back to start</a>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
