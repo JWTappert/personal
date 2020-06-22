c8096b9cb9618afe083d247ef9bab9cb32b22882
Tappert <tappertwj@gmail.com>
2020-06-22 07:42:49 -0700

    remove iframe

--- pages/_document.js
+++ pages/_document.js
@@ -44,14 +44,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-TMMVNJ9"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
           <Main />
           <NextScript />
         </body>
